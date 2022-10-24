e9a88446a2659ea0a4d5bf5652ab34f713a96c41
Thunderbird93 <antircristian@gmail.com>
2022-10-24 17:43:53 +0200

    [add] console log

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 //Palidroma
-const parolaInserita = "onorarono"
+const parolaInserita = prompt("")
 const parolaGirata = giraParola(parolaInserita.trim())
+const risultato = checkPalindroma(parolaInserita.trim(), parolaGirata)
+console.log(risultato);
+
+function giraParola(parola){
+    console.log(parola)
+    let nuovaParola = " ";
+    nuovaParola = parola.split(" ").reverse().join(" ");
+    return nuovaParola;
+}
+
+function checkPalindroma(parolaInserita, parolaGirata){
+    if(parolaInserita.toLowerCase() === parolaGirata.toLowerCase()) return "Palindroma";
+    return "Non Palindroma";
+}
