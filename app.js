c38f416ea7d6785d5b3da2bf0c2710d828d7a682
Thunderbird93 <antircristian@gmail.com>
2022-10-24 20:24:14 +0200

    [add] pari e dispari

--- app.js
+++ app.js
@@ -27,9 +27,25 @@ Dichiariamo chi ha vinto.
 */
 
 //L’utente sceglie pari o dispari
-let pari_dispari //= prompt('Scegli un numero Pari o Dispari')
+let pari_dispari; //= prompt('Scegli un numero Pari o Dispari')
 //inserisce un numero da 1 a 5.
-let numeroUtente //= prompt('Scegli un numero da 1 a 5')
+let numeroUtente = parseInt (prompt('Scegli un numero da 1 a 5'))
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 let numeroAi = Math.floor((Math.random()*5)+1);
+document.getElementById("numeroAi").innerHTML = numeroAi;
+document.getElementById("numeroUtente").innerHTML = numeroUtente;
+//Sommiamo i due numeri
+let somma = numeroAi + numeroUtente
+
+let risultato 
+
+
+if (somma % 2 === 0){
+    risultato = "Vince il Pari!"
+} else {
+    risultato = "Vince il Dispari!"
+}
+document.getElementById("risultato").innerHTML = risultato
+
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
